bd385b1fb6a264f0624db48b7f565c8047b86bdb
egborojie <egborojie337@gmail.com>
2023-06-13 16:47:29 +0100

    javascript

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let c = "C is fun"
+let python = "Python is cool"
+let js = "JavaScript is amazing"
+
+console.log(c);
+console.log(python);
+console.log(js);
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,4 @@
+<h1>An Introduction to Javascript</h1>
+ ---
+<p>ALX software Engineering</p>
+ 
